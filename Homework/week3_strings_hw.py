1012a68c7cbc34782b4ff23887f30621bb8986b7
nbrauser <nbrauser@gmail.com>
2024-01-31 17:09:07 -0500

    finish question 1 to 3

--- Homework/week3_strings_hw.py
+++ Homework/week3_strings_hw.py
@@ -1,3 +1,13 @@
+# Question 1: take an imput string from user and creat a new sring with the first, middle, last characters of the imput string
+
+first_string = input("Type a string \n")
+string_length = len(first_string)
+middle = (string_length/2)
+first_char = first_string[0]
+second_char = first_string[int(middle)]
+third_char = first_string[string_length - 1]
+print(first_char + second_char + third_char)
+
 # Problem 2
 firstname = str(input("What is your first name\n"))
 lastname = str(input("What is your last name \n"))
@@ -18,3 +28,9 @@ print("     SNN: " + str(SSN) + "\n")
 print("     Height: " + str(new_height) + "\n")
 print("     Weight: " + str(new_weight) + "\n")
 
+
+# problem 3
+Phrase = "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. -Dr. Seuss"
+print(Phrase[59:107])
+print(Phrase.find('feet'))
+print(Phrase.replace("Dr.Seuss", "Dr.Seuss, Oh,the Places You'll Go!"))
