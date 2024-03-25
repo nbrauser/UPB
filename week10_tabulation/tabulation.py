a12911d64fce15a579d0be0262cf7fb82b7ce94d
Brauser <nbrauser@gmail.com>
2024-03-25 14:25:50 -0400

    week 5

---
+++ week10_tabulation/tabulation.py
@@ -0,0 +1,24 @@
+def find_lcs(x, y):
+    len_x = len(x)
+    len_y = len(y)
+
+    lcs_array = []
+    for _ in range(len_x+1):
+        empty_row = [0] * (len_y+1)
+        lcs_array.append(empty_row)
+    for row in lcs_array:
+        print(row)
+    #  lcs_array1 = [[0]*len_y for _ in range(len_x+1)]
+
+    for row in range(1, len_x+1):
+        for col in range(1, len_y+1):
+            if x[row] == y[col]:
+                lcs_array[row][col] = 1 + lcs_array[row-1][col-1]
+            else:
+                lcs_array[row][col] = max(lcs_array[row-1][col], lcs_array[row][col-1])
+    return lcs_array[row][col]
+
+
+str1 = 'stone'
+str2 = 'longest'
+print(find_lcs(str1, str2))
