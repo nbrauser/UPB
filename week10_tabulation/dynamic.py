56a676934a11c9763941e7fff737745a98c7661e
nbrauser <nbrauser@gmail.com>
2024-03-27 14:25:19 -0400

    videos

--- week10_tabulation/dynamic.py
+++ week10_tabulation/dynamic.py
@@ -34,7 +34,7 @@ def fib_num_memo(req_fib_num):
     else:
         fib_val_memo[req_fib_num] = fib_num_memo(req_fib_num - 2) + fib_num_memo(req_fib_num - 1)
 
-        fib_val_memo = dict(list(fib_val_memo.items())[-2:])
+        fib_val_memo = dict(list(fib_val_memo.items())[-2:])  
     return fib_val_memo[req_fib_num]
 
 
@@ -52,4 +52,24 @@ for num in [5, 10, 15, 20, 25]:
     print()
 
 
+ite_call_count = 0
 
+
+def fib_ser(num_fib_terms):
+    global ite_call_count
+    ite_call_count += 1
+    if num_fib_terms <=0:
+        return
+    elif num_fib_terms == 1:
+        return[1]
+    elif num_fib_terms == 2:
+        return [1, 1]
+    else:
+        fib_list = [1, 1]
+        for i in range (2, num_fib_terms):
+            fib_list.append(fib_list[i-a] + fib_list[i - 2])
+        return fib_list
+
+
+print(fib_ser(40))
+print(ite_call_count)
